c02ea79a793e8c9700d5097831a8f37fc831b469
medrom4 <medikroma@gmail.com>
2019-06-01 13:07:34 +0300

    add-oKompanii-video

--- js/layout.js
+++ js/layout.js
@@ -1,91 +1,91 @@
 var Layout = function () {
-    'use strict';
-    
-    // handle on page scroll
-    var handleHeaderOnScroll = function() {
-        if ($(window).scrollTop() > 60) {
-            $('body').addClass('page-on-scroll');
-        } else {
-            $('body').removeClass('page-on-scroll');
-        }
-    }   
+	'use strict';
 
-    // handle carousel
-    var handleCarousel = function() {
-        var $item = $('.carousel .item'); 
-        var $wHeight = $(window).height();
-        $item.eq(0).addClass('active');
-        $item.height($wHeight); 
-        $item.addClass('full-screen');
+	// handle on page scroll
+	var handleHeaderOnScroll = function () {
+		if ($(window).scrollTop() > 60) {
+			$('body').addClass('page-on-scroll');
+		} else {
+			$('body').removeClass('page-on-scroll');
+		}
+	}
 
-        $('.carousel img').each(function() {
-            var $src = $(this).attr('src');
-            var $color = $(this).attr('data-color');
-            $(this).parent().css({
-                'background-image' : 'url(' + $src + ')',
-                'background-color' : $color
-            });
-            $(this).remove();
-        });
+	// handle carousel
+	var handleCarousel = function () {
+		var $item = $('.carousel .item');
+		var $wHeight = $(window).height();
+		$item.eq(0).addClass('active');
+		$item.height($wHeight);
+		$item.addClass('full-screen');
 
-        $(window).on('resize', function (){
-            $wHeight = $(window).height();
-            $item.height($wHeight);
-        });
-    }
+		$('.carousel img').each(function () {
+			var $src = $(this).attr('src');
+			var $color = $(this).attr('data-color');
+			$(this).parent().css({
+				'background-image': 'url(' + $src + ')',
+				'background-color': $color
+			});
+			$(this).remove();
+		});
 
-    // handle group element heights
-    var handleHeight = function() {
-       $('[data-auto-height]').each(function() {
-            var parent = $(this);
-            var items = $('[data-height]', parent);
-            var height = 0;
-            var mode = parent.attr('data-mode');
-            var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
+		$(window).on('resize', function () {
+			$wHeight = $(window).height();
+			$item.height($wHeight);
+		});
+	}
 
-            items.each(function() {
-                if ($(this).attr('data-height') == "height") {
-                    $(this).css('height', '');
-                } else {
-                    $(this).css('min-height', '');
-                }
+	// handle group element heights
+	var handleHeight = function () {
+		$('[data-auto-height]').each(function () {
+			var parent = $(this);
+			var items = $('[data-height]', parent);
+			var height = 0;
+			var mode = parent.attr('data-mode');
+			var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
 
-                var height_ = (mode == 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
-                if (height_ > height) {
-                    height = height_;
-                }
-            });
+			items.each(function () {
+				if ($(this).attr('data-height') == "height") {
+					$(this).css('height', '');
+				} else {
+					$(this).css('min-height', '');
+				}
 
-            height = height + offset;
+				var height_ = (mode == 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
+				if (height_ > height) {
+					height = height_;
+				}
+			});
 
-            items.each(function() {
-                if ($(this).attr('data-height') == "height") {
-                    $(this).css('height', height);
-                } else {
-                    $(this).css('min-height', height);
-                }
-            });
+			height = height + offset;
 
-            if(parent.attr('data-related')) {
-                $(parent.attr('data-related')).css('height', parent.height());
-            }
-       });
-    }
+			items.each(function () {
+				if ($(this).attr('data-height') == "height") {
+					$(this).css('height', height);
+				} else {
+					$(this).css('min-height', height);
+				}
+			});
 
-    return {
-        init: function () {
-            handleHeaderOnScroll(); // initial setup for fixed header
-            handleCarousel(); // initial setup for carousel
-            handleHeight(); // initial setup for group element height
+			if (parent.attr('data-related')) {
+				$(parent.attr('data-related')).css('height', parent.height());
+			}
+		});
+	}
 
-            // handle minimized header on page scroll
-            $(window).scroll(function() {
-                handleHeaderOnScroll();
-            });
-        }
-    };
+	return {
+		init: function () {
+			handleHeaderOnScroll(); // initial setup for fixed header
+			handleCarousel(); // initial setup for carousel
+			handleHeight(); // initial setup for group element height
+
+			// handle minimized header on page scroll
+			$(window).scroll(function () {
+				handleHeaderOnScroll();
+			});
+		}
+	};
 }();
 
-$(document).ready(function() {
-    Layout.init();
+$(document).ready(function () {
+	Layout.init();
 });
